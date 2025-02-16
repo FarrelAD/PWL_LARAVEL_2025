06fe43276fd887a1db387c2aee0165a843aa3edf
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-16 21:30:56 +0700

    add simple implementation of render view in controller method

--- routes/web.php
+++ routes/web.php
@@ -80,6 +80,4 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function () {
-    return view('blog.hello', ['name' => 'Andi']);
-});
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
