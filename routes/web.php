f3c6852cdb53c8f8dc3323389bb66e201c4564bc
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-16 20:20:49 +0700

    add some new simple route to learn basic laravel routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Selamat datang!';
+});
+
+Route::get('/hello', function() {
+    return 'Hello World!';
+});
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/about', function() {
+    return '
+    <h1>About me:</h1>
+    <ul>
+        <li>Name: Farrel Augusta Dinata</li>
+        <li>NIM: 2341720081</li>';
 });
